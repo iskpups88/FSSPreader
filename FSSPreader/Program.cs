18fef016be9b31f5d5326554bdfc81b40b749a47
Искандер Хакимжанов <i.khakimzhanov@bars-open.ru>
2019-01-29 19:19:23 +0300

    init

--- FSSPreader/Program.cs
+++ FSSPreader/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FSSPreader
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            List<Row> rows = new List<Row>();
+
+            using (var reader = new StreamReader(@"C:\Users\i.khakimzhanov\Desktop\Выгрузка\Дети ФССП_РТ1.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split('|');
+                    Row current = new Row
+                    {
+                        FullName = values[0],
+                        OSP = values[1],
+                        BirthDate = Convert.ToDateTime(values[2]),
+                        BirthPlace = values[3],
+                        Address = values[4],
+
+                        DebtorFullName = values[5],
+                        DebtorBirthDate = string.IsNullOrWhiteSpace(values[6]) ? new DateTime(1000, 1, 1) : DateTime.ParseExact(values[6], "yyyy-MM-dd", CultureInfo.InvariantCulture),
+                        DebtorAddress = values[7],
+
+                        СlaimantFullName = values[8],
+                        ClaimantBirthDate = string.IsNullOrWhiteSpace(values[9]) ? new DateTime(1000, 1, 1) : DateTime.ParseExact(values[9], "yyyy-MM-dd", CultureInfo.InvariantCulture),
+                        СlaimanAddress = values[10],
+                        //IpId = decimal.Parse(values[11], CultureInfo.InvariantCulture),
+                        //IpNumber = decimal.Parse(values[12], CultureInfo.InvariantCulture),
+                        IpStatus = values[13]
+                    };                    
+                    using (StreamWriter file = new StreamWriter(@"C:\Users\i.khakimzhanov\Desktop\Выгрузка\Ready.csv", true))
+                    {
+                        file.WriteLine(current.ToString());
+                    }
+                }
+            }
         }
     }
 }
---
+++ FSSPreader/Row.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSSPreader
+{
+    public class Row
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Partonymic { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return Surname + " " + Name + " " + Partonymic;
+            }
+            set
+            {
+                string[] splitArr = value.Split(' ');
+                Surname = splitArr[0].Trim();
+                Name = splitArr[1].Trim();
+                Partonymic = splitArr[2].Trim();
+            }
+        }
+        public string OSP { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string BirthPlace { get; set; }
+        public string Address { get; set; }
+        public string DebtorFullName { get; set; }
+        public DateTime DebtorBirthDate { get; set; }
+        public string DebtorAddress { get; set; }
+        public string СlaimantFullName { get; set; }
+        public DateTime ClaimantBirthDate { get; set; }
+        public string СlaimanAddress { get; set; }
+
+        public decimal IpId { get; set; }
+        public decimal IpNumber { get; set; }
+        public string IpStatus { get; set; }
+
+        public int isDisabled { get; set; } = 0;
+
+        public override string ToString()
+        {
+            return
+            $@"{Surname.Trim()}|{Name.Trim()}|{Partonymic.Trim()}|{OSP.Trim()}|{BirthDate}|{BirthPlace.Trim()}|{Address}|{DebtorFullName.Trim()}|
+               {DebtorBirthDate}|{DebtorAddress.Trim()}|{СlaimantFullName.Trim()}|{ClaimantBirthDate}|{СlaimanAddress.Trim()}|
+               {IpId}|{IpNumber}|{IpStatus.Trim()}|{isDisabled}|";
+        }
+    }
+}
